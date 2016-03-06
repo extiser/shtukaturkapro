26940a8f176a6edc1d32c7c268c3df807f9f4dd0
extiser <1sappho@gmail.com>
2016-03-06 18:50:20 +0500

    new repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# shtukaturkapro
-Shtukaturkapro.ru
--- markup/modules/top-menu/top-menu.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('.top-menu__mobile').click(function () {
-        $('.top-menu__mobile, .top-menu__fixed, body, .mask').addClass('active');
-    });
-
-    $('.mask').click(function () {
-        $('.top-menu__mobile, .top-menu__fixed, body, .mask').removeClass('active');
-    });
-});
